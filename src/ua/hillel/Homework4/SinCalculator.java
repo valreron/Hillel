40c272fce00137b4bb2919aa6c9399ba97604fcf
valreron <43524964+valreron@users.noreply.github.com>
2022-10-30 10:31:52 +0200

    Homework4 sin() exer

---
+++ src/ua/hillel/Homework4/SinCalculator.java
@@ -0,0 +1,10 @@
+package ua.hillel.Homework4;
+
+public class SinCalculator {
+    public static void main(String[] args) {
+        for(int i = 0;i<=360;){
+            System.out.println("Sin of " + i + " = " + Math.sin(i));
+            i = i+10;
+        }
+    }
+}
