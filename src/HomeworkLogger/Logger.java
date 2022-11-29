3de1ca677a86a1fa0dad608fdd7b8d73d0619f65
valreron <43524964+valreron@users.noreply.github.com>
2022-11-29 20:57:17 +0200

    Logger move Color class to Logger

--- src/HomeworkLogger/Logger.java
+++ src/HomeworkLogger/Logger.java
@@ -27,4 +27,28 @@ public class Logger {
         }
     }
 
+    enum Color {
+        RESET("\u001B[0m"),
+        BLACK("\u001B[30m"),
+        RED("\u001B[31m"),
+        GREEN("\u001B[32m"),
+        YELLOW("\u001B[33m"),
+        BLUE("\u001B[34m"),
+        PURPLE("\u001B[35m"),
+        CYAN("\u001B[36m"),
+        WHITE("\u001B[37m");
+
+        private String color;
+
+        Color(String logger) {
+
+            this.color = logger;
+        }
+
+        public String getColor() {
+
+            return color;
+        }
+
+    }
 }
